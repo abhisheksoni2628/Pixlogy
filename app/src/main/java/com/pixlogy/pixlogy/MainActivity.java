199114cbb119634e0f62e104a10f8e74c2bbdb9e
Abhishek Soni <87596941+abhisheksoni2628@users.noreply.github.com>
2022-11-21 11:38:43 +0530

    Splash init

--- app/src/main/java/com/pixlogy/pixlogy/MainActivity.java
+++ app/src/main/java/com/pixlogy/pixlogy/MainActivity.java
@@ -2,7 +2,9 @@ package com.pixlogy.pixlogy;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.WindowManager;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,27 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        Thread th = new Thread() {
+            @Override
+            public void run() {
+                try {
+                    sleep(5000);
+
+                } catch (Exception ex) {
+                    ex.printStackTrace();
+
+                } finally {
+
+                        Intent intent = new Intent(MainActivity.this, Home.class);
+                        startActivity(intent);
+                        finish();
+
+                    }
+                }
+        };
+        th.start();
+
     }
 }
