f683e4a3f7edffc56ab0d5cb54e88893fac561b6
kushgarg27 <76466199+kushgarg27@users.noreply.github.com>
2023-01-21 12:04:17 +0530

    created files

---
+++ app/src/main/java/com/pixlogy/pixlogy/auth/login.java
@@ -0,0 +1,16 @@
+package com.pixlogy.pixlogy.auth;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.pixlogy.pixlogy.R;
+
+public class login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
