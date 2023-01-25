c43140d54d318f3d2c73e05834e2210ba9ad7b2d
kushgarg27 <76466199+kushgarg27@users.noreply.github.com>
2023-01-25 11:04:36 +0530

    created files

--- app/src/main/java/com/pixlogy/pixlogy/Home.java
+++ app/src/main/java/com/pixlogy/pixlogy/Home.java
@@ -1,14 +1,18 @@
 package com.pixlogy.pixlogy;
 
+import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.pixlogy.pixlogy.auth.Login;
 
 public class Home extends AppCompatActivity {
     Button Signout;
@@ -24,15 +28,17 @@ public class Home extends AppCompatActivity {
             window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
             window.setStatusBarColor(this.getResources().getColor(R.color.theme));
             mAuth = FirebaseAuth.getInstance();
-//            Signout = findViewById(R.id.signout);
-//            Signout.setOnClickListener(new View.OnClickListener() {
-//                @Override
-//                public void onClick(View v) {
-//                    mAuth.signOut();
-//                    startActivity(new Intent(Home.this, Login.class));
-//
-//                }
-//            });
+            Signout = findViewById(R.id.signout);
+            Signout.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    mAuth.signOut();
+
+                    startActivity(new Intent(Home.this, Login.class));
+                    Toast.makeText(Home.this, "Log out successful", Toast.LENGTH_SHORT).show();
+
+                }
+            });
        }
         }
     }
