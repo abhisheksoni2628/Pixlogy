645bd6733230ffd5f6ae56205314d62629f55805
kushgarg27 <76466199+kushgarg27@users.noreply.github.com>
2023-02-20 09:48:39 +0530

    Phone Authentication complete

--- app/src/main/java/com/pixlogy/pixlogy/auth/Otp.java
+++ app/src/main/java/com/pixlogy/pixlogy/auth/Otp.java
@@ -1,16 +1,41 @@
 package com.pixlogy.pixlogy.auth;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.PhoneAuthCredential;
+import com.google.firebase.auth.PhoneAuthProvider;
+import com.pixlogy.pixlogy.Home;
 import com.pixlogy.pixlogy.R;
 
 public class Otp extends AppCompatActivity {
+    TextView phone, resendOtp ;
+    EditText otp;
+    Button verfiyOtp;
+    ProgressBar progressBar;
 
+    String VerficationID;
+
+
+
+    @SuppressLint("SetTextI18n")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,6 +45,61 @@ public class Otp extends AppCompatActivity {
             window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
             window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
             window.setStatusBarColor(this.getResources().getColor(R.color.theme));
+            phone = findViewById(R.id.phoneNUm);
+            otp = findViewById(R.id.et_otp);
+            verfiyOtp = findViewById(R.id.btn_verify);
+            resendOtp = findViewById(R.id.tv_resend);
+            progressBar = findViewById(R.id.progressbar);
+            phone.setText("+91" + getIntent().getStringExtra("phone"));
+            VerficationID = getIntent().getStringExtra("verficationId");
+            resendOtp.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+
+                    Toast.makeText(Otp.this, "otp resend successfully ", Toast.LENGTH_SHORT).show();
+                }
+            });
+            verfiyOtp.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    progressBar.setVisibility(View.VISIBLE);
+                    verfiyOtp.setVisibility(View.INVISIBLE);
+                    if(otp.getText().toString().trim().isEmpty()){
+                        otp.setError("otp ccant be empty");
+                        otp.requestFocus();
+                    }
+                    else{
+                        if(VerficationID!= null){
+                            String code = otp.getText().toString().trim();
+                            PhoneAuthCredential credential = PhoneAuthProvider.getCredential(VerficationID, code);
+                            FirebaseAuth.getInstance().signInWithCredential(credential).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                                @Override
+                                public void onComplete(@NonNull Task<AuthResult> task) {
+                                 if (task.isSuccessful()){
+                                     Intent intent = new Intent(Otp.this , Home.class);
+                                     intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK| Intent.FLAG_ACTIVITY_NEW_TASK);
+                                     progressBar.setVisibility(View.VISIBLE);
+                                     verfiyOtp.setVisibility(View.INVISIBLE);
+
+                                     startActivity(intent);
+
+                                 }
+                                 else{
+                                     progressBar.setVisibility(View.GONE);
+                                     verfiyOtp.setVisibility(View.VISIBLE);
+                                     Toast.makeText(Otp.this, "otp not valid", Toast.LENGTH_SHORT).show();
+
+                                 }                                }
+                            });
+                        }
+
+                    }
+                }
+            });
+
+
+
+
         }
     }
 }
--- app/src/main/java/com/pixlogy/pixlogy/auth/phoneNum.java
+++ app/src/main/java/com/pixlogy/pixlogy/auth/phoneNum.java
@@ -1,21 +1,41 @@
 package com.pixlogy.pixlogy.auth;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.FirebaseException;
+import com.google.firebase.FirebaseTooManyRequestsException;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.PhoneAuthCredential;
+import com.google.firebase.auth.PhoneAuthOptions;
+import com.google.firebase.auth.PhoneAuthProvider;
 import com.pixlogy.pixlogy.R;
+
+import java.util.concurrent.TimeUnit;
 
 public class phoneNum extends AppCompatActivity {
     Button Phone;
     EditText phonenumber;
+    ProgressBar progressBar;
+    private FirebaseAuth mAuth;
+    private PhoneAuthProvider.OnVerificationStateChangedCallbacks mCallbacks;
 
 
     @Override
@@ -29,6 +49,9 @@ public class phoneNum extends AppCompatActivity {
             window.setStatusBarColor(this.getResources().getColor(R.color.theme));
             Phone = findViewById(R.id.loginBtn);
             phonenumber = findViewById(R.id.et_phone);
+            progressBar = findViewById(R.id.progressbar);
+            mAuth = FirebaseAuth.getInstance();
+
             Phone.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
@@ -41,8 +64,69 @@ public class phoneNum extends AppCompatActivity {
                         phonenumber.requestFocus();
 
                     }
+                    else{
+                        sendOtp();
+
+                    }
                 }
             });
         }
     }
-}
+
+    private void sendOtp() {
+
+
+
+            progressBar.setVisibility(View.VISIBLE);
+            Phone.setVisibility(View.INVISIBLE);
+
+
+            mCallbacks = new PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
+
+                @Override
+                public void onVerificationCompleted(@NonNull PhoneAuthCredential credential) {
+
+
+
+
+                }
+
+                @Override
+                public void onVerificationFailed(@NonNull FirebaseException e) {
+
+                    progressBar.setVisibility(View.GONE);
+
+                    Phone.setVisibility(View.VISIBLE);
+                    Toast.makeText(phoneNum.this, e.getLocalizedMessage(), Toast.LENGTH_SHORT).show();
+
+
+
+
+                }
+
+                @Override
+                public void onCodeSent(@NonNull String verificationId,
+                                       @NonNull PhoneAuthProvider.ForceResendingToken token) {
+                    progressBar.setVisibility(View.GONE);
+                    Phone.setVisibility(View.VISIBLE);
+                    Intent i = new Intent(phoneNum.this,Otp.class);
+                    i.putExtra("phone",phonenumber.getText().toString().trim());
+                    i.putExtra("verficationId",verificationId);
+                    startActivity(i);
+
+
+                }
+            };
+            PhoneAuthOptions options =
+                    PhoneAuthOptions.newBuilder(mAuth)
+                            .setPhoneNumber("+91"+phonenumber.getText().toString())       // Phone number to verify
+                            .setTimeout(60L, TimeUnit.SECONDS) // Timeout and unit
+                            .setActivity(this)                 // Activity (for callback binding)
+                            .setCallbacks(mCallbacks)          // OnVerificationStateChangedCallbacks
+                            .build();
+            PhoneAuthProvider.verifyPhoneNumber(options);
+
+        }
+    }
+
+
